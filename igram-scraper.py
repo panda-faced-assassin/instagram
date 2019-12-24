02102ce4f8028dbdb01247a9b22d1de3885601d1
Matt Wayne <matt@thinkdolabs.com>
2019-12-24 05:09:36 -0800

    Message.

--- igram-scraper.py
+++ igram-scraper.py
@@ -1,9 +1,12 @@
-from igramscraper.instagram import Instagram 
+from igramscraper.instagram import Instagram
+from time import sleep
 
 instagram = Instagram()
+sleep(2)
 
-subjects = ['berniesanders']
+subjects = ['berniesanders', 'realdonaldtrump']
 
+# Get account objects
 def get_accounts():
     accounts = []
     for subject in subjects:
@@ -11,24 +14,44 @@ def get_accounts():
         accounts.append(account)
     return accounts
 
-def get_account_data():
-# Create data dict
+# Get account data
+def get_account_data(account):
     account_data_dict = {}
-# Create object variable
-    accounts = get_accounts()
-# Create {key: value} pairs
-    for account in accounts:
-        account_data_dict['account_external_url'] = account.external_url
-        account_data_dict['account_id'] = account.identifier
-        account_data_dict['account_is_verified'] = account.is_verified
-        account_data_dict['account_username'] = account.username 
-        account_data_dict['account_full_name'] = account.full_name
-        account_data_dict['account_bio'] = account.biography
-        account_data_dict['account_profile_pic_url'] = account.get_profile_picture_url()
-        account_data_dict['account_num_published_posts'] = account.media_count
-        account_data_dict['account_num_followers'] = account.followed_by_count
-        account_data_dict['account_num_follows'] = account.follows_count
+    account_data_dict['account_external_url'] = account.external_url
+    account_data_dict['account_id'] = account.identifier
+    account_data_dict['account_is_verified'] = account.is_verified
+    account_data_dict['account_username'] = account.username 
+    account_data_dict['account_full_name'] = account.full_name
+    account_data_dict['account_bio'] = account.biography
+    account_data_dict['account_profile_pic_url'] = account.get_profile_picture_url()
+    account_data_dict['account_num_published_posts'] = account.media_count
+    account_data_dict['account_num_followers'] = account.followed_by_count
+    account_data_dict['account_num_follows'] = account.follows_count
     return account_data_dict
 
-account_data = get_account_data()
-print(account_data)
+# account_data_dicts = []
+# accounts = get_accounts() 
+# for account in accounts: 
+#     account_data = get_account_data(account)
+#     account_data_dicts.append(account_data)
+# print(account_data_dicts)
+
+# Get follower data
+def get_followers(account):
+    account_followers = {}
+    for subject in subjects:
+        username = subject
+        sleep(1)
+        followers = instagram.get_followers(account.identifier, 10, 1, delayed=True) # Get 10 followers of 'subject', 1 at a time with random delay in between requests. 
+        account_followers[subject] = followers
+    return account_followers
+
+account_followers_dicts = []
+accounts = get_accounts() 
+for account in accounts: 
+    account_followers = get_followers(account)
+    account_followers_dicts.append(account_followers)
+print(account_followers_dicts)
+
+
+
